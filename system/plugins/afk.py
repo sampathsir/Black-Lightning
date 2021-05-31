69906e12e809a5b67f077988f7d3156acd6c39fd
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-31 20:12:59 +0530

    Update afk.py

--- system/plugins/afk.py
+++ system/plugins/afk.py
@@ -1,53 +1,83 @@
-# # # Copyright (C) 2021 KeinShin@Github.
+ # Copyright (C) 2021 KeinShin@Github. All rights reserved
+
+
+import asyncio
+from pyrogram.raw.types.message import Message
+from pyrogram.types import update
+from system.datas_sqlite.afk_sqlite import get_afk, get_reason, update_afk, del_afk
+from system.datas_sqlite import c, conn
+
+from system import COMMAND_HELP, OWNER, light, HNDLR
+from system.Config import Variable
+from system import (
+  app,
+)
+
+from pyrogram import filters
 
 
 
-# from system import light
-# from system.Config import Variable
-# from system import (
-#   app,
-#   g,
-#   bot,
-#   a,
-# )
-# from system.Config.utils import language
+@light.on(["afk"])
 
-# from pyrogram import filters
-# # create = open("afk.txt", "a")
-# # file = open("afk.txt", "r")
-# # if "False" in file:
-# #       txt = "False"
-# #       txs = "True"
-# # else:
-# #     txt = "Ture"
-# #     txs  = "False"
+async def _(client, message ):
 
-# # file.close()
+    try:
 
+     if get_afk():
+         return
+    except IndexError:
+        pass
 
-# @light.on(["afk"])
-
-# async def _(client, message ):
-  
-
-# @app.on_message(filters.mentioned & filters.incoming & ~filters.bot & filters.private)
-# async def n(client, message):
-#   file = open("afk.txt", "r")
-# #   if "False" in file:
-# #     return
-# #   own = await app.get_me()
-# #   user = await app.get_users(int(own.id))
-# #   if " " in a:
+    try:
+        txt = message.text
+        reason = txt.split()[1:]
+        reason=" ".join(reason)
+    except IndexError:
+        reason = "**Contact me when i'm back alive, till [AFK]**\n __This is an automated message__"
         
-# #    res = message.text.split()[1]
-# #   else:
-# #    res = ""
-# #   if res:
-# #       text = language("**Master is afk from** {}\n\n~Reason~ - __{}__".format(user.last_online_date, res)) 
+    update_afk("True", reason)
+    
+    await message.edit(f"**Afk update from now onwards i'll handle any kind of updates**\n\n**Reason** - {reason}")
 
-# #   else:
-# #       text = language(f"__I'm afk till i'm done with my tasks contact me via {g}")
-# #   await app.send_message(message.chat.id, f"__{text}__")
+@app.on_message(~filters.bot & (filters.mentioned | filters.private) & ~filters.user('keinshin'))
+async def n(client, message):
+  user = await app.get_me()
+  usr = await app.get_users(user.id)
+  if not get_afk():
+    
+    return
 
 
+  ar = "**Contact me when i'm back alive, till [AFK]**\n __This is an automated message__"
+  if get_reason() != ar:
+        msg=f"""
+**Hello User @{message.from_user.username}!
+This is an automated message from my assistant because I'm [AFK]**
+__I was Last online __ - {user.status}
+**Reason for afk**:
+ 
+    {get_reason()}
+""" 
+  else:
+       msg = get_reason()
+     
+  await app.send_message(message.chat.id, text=msg, reply_to_message_id=message.message_id)
 
+
+@app.on_message(filters.user(OWNER))
+async def sed(client, message:Message):
+     
+    try:
+
+     del_afk()
+    
+     msg=await app.send_message(message.chat.id, "**AFK BACK NORMAL [ONLINE]**" )
+     await asyncio.sleep(1)
+     await app.delete_messages(msg.chat.id, msg.message_id, revoke=True)
+    except Exception:
+        pass
+
+COMMAND_HELP.update({
+    "afk": f"`{HNDLR} afk` `(reason) or default`",
+    "afk's help": "**USE**: __Creates an automated message by your bot when you are afk__"
+})
